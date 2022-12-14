851f0681e6b3123fc7a0e3a5dd3bd0d167cbf73e
AnDoFSE <114121379+AnDoFSE@users.noreply.github.com>
2022-12-14 03:39:26 -0600

    Primer Login

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/login-gtst'),
+      dir: require('path').join(__dirname, './coverage/login'),
       subdir: '.',
       reporters: [
         { type: 'html' },
